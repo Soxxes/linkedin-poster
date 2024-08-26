915eaecb432e394ecb5bdab4684e3fbb32c66e1f
Soxxes <marc_anmeldungen@proton.me>
2024-08-26 16:29:08 +0200

    updated runner script

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
-"""
-Main Runner Script
-"""
+import streamlit as st
+
+from sections.landing import landing_page
+from sections.questions import questionnaire_page
+from sections.result import result_page
+
+
+if 'current_page' not in st.session_state:
+    st.session_state.current_page = "landing"
+if 'current_group_index' not in st.session_state:
+    st.session_state.current_group_index = 1
+if 'responses' not in st.session_state:
+    st.session_state.responses = {}
+
+# page routing
+if st.session_state.current_page == "landing":
+    landing_page()
+elif st.session_state.current_page == "questionnaire":
+    questionnaire_page()
+elif st.session_state.current_page == "results":
+    result_page()
